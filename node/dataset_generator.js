02e2e0d01213f9af79a6c00a05d7abfd7fec4ee9
riyanswat <riyanswat@gmail.com>
2023-09-14 23:17:30 +0500

    added data generator and more ...

---
+++ node/dataset_generator.js
@@ -0,0 +1,28 @@
+const constants = {};
+
+constants.DATA_DIR = "../data";
+constants.RAW_DIR = `${constants.DATA_DIR}/raw`;
+constants.DATASET_DIR = `${constants.DATA_DIR}/dataset`;
+constants.JSON_DIR = `${constants.DATASET_DIR}/json`;
+constants.IMG_DIR = `${constants.DATASET_DIR}/img`;
+constants.SAMPLES = `${constants.DATASET_DIR}/samples.json`;
+
+const fs = require("fs");
+const fileNames = fs.readdirSync(constants.RAW_DIR);
+const samples = [];
+let id = 1;
+fileNames.forEach((fn) => {
+  const content = fs.readFileSync(`${constants.RAW_DIR}/${fn}`);
+  const { session, student, drawings } = JSON.parse(content);
+  for (let label in drawings) {
+    samples.push({
+      id,
+      label,
+      student_name: student,
+      student_id: session,
+    });
+    id++;
+  }
+});
+
+fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples));
