44f902af4201669b0544a2a5b6690ed40d5998a4
riyanswat <riyanswat@gmail.com>
2023-09-22 01:03:33 +0500

    updated

--- common/utils.js
+++ common/utils.js
@@ -15,6 +15,8 @@ utils.styles = {
   clock: { color: "lightgray", text: "⏰" },
 };
 
+utils.styles["?"] = { color: "red", text: "❓" };
+
 utils.formatPercent = (n) => {
   return (n * 100).toFixed(2) + "%";
 };
